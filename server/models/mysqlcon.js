5fc085e56a0734af8cf2ba26ab849c52893441a7
m81923@gmail.com <JunHong Lin>
2021-05-19 15:33:05 +0800

    Create mySQL connection pool in model

--- server/models/mysqlcon.js
+++ server/models/mysqlcon.js
@@ -1,1 +1,15 @@
 require("dotenv").config();
+const mysql = require("mysql");
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  connectionLimit: 20,
+  multipleStatements: true
+});
+
+module.exports = {
+  pool
+};
