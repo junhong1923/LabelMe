b48cfc6690c005aff2bbe8357d5b49a14167706f
m81923@gmail.com <JunHong Lin>
2021-05-21 15:06:20 +0800

    Save image information into image table, and update user table as well

--- server/controllers/label_controller.js
+++ server/controllers/label_controller.js
@@ -1,11 +1,13 @@
 const Label = require("../models/label_model");
 
-const getOriImage = (req, res) => {
-  console.log("controller");
-  console.log(req.user);
-  console.log(req.file);
-  res.status(200).send("got it");
-  // to-do: 先驗證身份、存檔名或路徑到sql
+const getOriImage = async (req, res) => {
+  const userId = req.user.id;
+  const imgSize = (req.file.size / 1024).toFixed(2); // 原始單位：bytes，先換算成kb
+  const imgPath = req.file.location;
+  const result = await Label.insertOriginalImage(userId, imgSize, imgPath);
+  if (result.changedRows === 1) {
+    res.status(200).json({ userId, imgSize, imgPath });
+  }
 };
 
 const getCoordinates = (req, res) => {
--- server/models/label_model.js
+++ server/models/label_model.js
@@ -1,5 +1,47 @@
 const { pool } = require("./mysqlcon");
 
+const insertOriginalImage = (userId, imgSize, imgPath) => {
+  return new Promise((resolve, reject) => {
+    console.log(userId, imgSize, imgPath);
+    pool.getConnection((err, conn) => {
+      if (err) reject(err);
+      conn.beginTransaction((err) => {
+        if (err) {
+          conn.rollback(() => { conn.release(); });
+          reject(err);
+        } else {
+          // 1. insert userId, imgPath into original_image
+          conn.query("INSERT INTO original_image SET image_path = ?, user_id = ?", [imgPath, userId], (err, result) => {
+            if (err) {
+              conn.rollback(() => { conn.release(); });
+              reject(err);
+            } else {
+              // 2. insert imgSize(capacity累加) and update qty in user
+              conn.query("UPDATE user SET img_qty = img_qty + 1, capacity = capacity + ? WHERE id = ?", [imgSize, userId], (err, result) => {
+                if (err) {
+                  conn.rollback(() => { conn.release(); });
+                  reject(err);
+                } else {
+                  conn.commit((err) => {
+                    if (err) {
+                      conn.rollback(() => { conn.release(); });
+                      reject(err);
+                    } else {
+                      // Success
+                      conn.release();
+                      resolve(result);
+                    }
+                  });
+                }
+              });
+            }
+          });
+        }
+      });
+    });
+  });
+};
+
 const insertCoordinates = (coordinates) => {
   return new Promise((resolve, reject) => {
     pool.query("INSERT INTO label_result SET type = ?, coordinates_xy = point(?,?), coordinates_wh = point(?,?), user_id = ?, image_id = ?", [coordinates.type, coordinates.x, coordinates.y, coordinates.width, coordinates.height, "test", 1], (err, result) => {
@@ -10,5 +52,6 @@ const insertCoordinates = (coordinates) => {
 };
 
 module.exports = {
+  insertOriginalImage,
   insertCoordinates
 };
