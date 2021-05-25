40c84d089e5bd043e2980f4d1064c3c41be33bb4
junhong1923 <m81923@gmail.com>
2021-05-25 12:40:12 +0800

    Concatenate image API with index.html

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,31 @@
-const navLogin = document.getElementById("nav-login");
-const navLabel = document.getElementById("nav-label");
+// const navLogin = document.getElementById("nav-login");
+// const navLabel = document.getElementById("nav-label");
+const imageRow = document.querySelector(".album .container .row");
 
 window.onload = (e) => {
   const token = localStorage.getItem("token");
-  console.log(token);
+  fetch("/api/1.0/images/public?tag=0", {
+    method: "GET"
+  })
+    .then((res) => {
+      if (res.status === 200) {
+        return res.json();
+      } else { console.log(res.status); }
+    })
+    .then((res) => {
+      console.log(res);
+      res.forEach(obj => {
+        const imgPath = obj.image_path;
+        const html = `
+        <div class="col-4">
+            <div class="card shadow-sm">
+                <a>
+                    <img src=${imgPath} width="100%" height="100%">
+                </a>
+            </div>
+        </div>
+        `;
+        imageRow.innerHTML += html;
+      });
+    });
 };
