0ea9c5566968dcf105a27dd3d147abcc84742ffe
junhong1923 <m81923@gmail.com>
2021-05-31 11:07:28 +0800

    Add create folder function in private view

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,6 @@
+// import Swal from "sweetalert2";
+// const Swal = require("sweetalert2");
 const imageRow = document.querySelector(".album .container .row");
-const imagesBox = document.querySelector(".pictures");
 const filterTab = document.querySelector("#myTab");
 const selectedTabContent = document.querySelector("#myTabContent");
 let userId;
@@ -27,37 +28,93 @@ const getImageData = (type, userId, status) => {
     //   console.log(res);
       if (!res.error) {
         imageRow.innerHTML = "";
-        res.forEach(obj => {
-          const imgHref = `html/label.html?id=${obj.image_id}&src=${obj.image_path}`;
-          const imgPath = obj.image_path;
-          const labelStatus = obj.status ? "labeled" : "unlabeled";
-          const html = `
-              <div class="col-4">
-                  <div class="card shadow-sm">
-                      <a class="image" href=${imgHref}>
-                          <img src=${imgPath} width="100%" height="100%">
-                      </a>
-                      <p>Tag: ${obj.tag}</p>
-                      <p>Status: ${labelStatus}</p>
-                  </div>
-              </div>
-              `;
-          imageRow.innerHTML += html;
 
-          // using css only
-          // const html = `
-          // <div class="picture">
-          //     <p>Tag: ${obj.tag}</p>
-          //     <div class="imageContainer">
-          //         <a class="image" href=${imgHref}>
-          //             <img src=${imgPath} alt="">
-          //         </a>
-          //     </div>
-          //     <p>Download Image: <a target="_blank" href=${imgPath}>Click Here</a></p>
-          // </div>
-          // `;
-          // imagesBox.innerHTML += html;
-        });
+        if (type === "private") {
+          imageRow.innerHTML = `
+            <div class="folder">
+              <ul class="list-unstyled ps-0">
+                <a href="/" class="d-flex align-items-center pb-3 mb-3 link-dark text-decoration-none border-bottom">
+                  <img src="../images/member.png" alt="mdo" width="32" height="32" class="rounded-circle"/>
+                  <span class="fs-5 fw-semibold">My Data</span>
+                </a>
+                <li class="mb-1">
+                  <button id="createFolder" class="btn btn-toggle align-items-center rounded collapsed">+ Create Folder</button>
+                </li>
+              </ul>
+            </div>
+
+            <div class="row images"></div>        
+          `;
+
+          // eventlistener for create folder btn
+          const createFolderBtn = document.querySelector("#createFolder");
+          createFolderBtn.onclick = (e) => {
+            const folder = document.createElement("button");
+
+            Swal.fire({
+              title: "Type folder name",
+              input: "text",
+              showCancelButton: true
+            });
+
+            folder.className = "btn";
+            folder.textContent = "Folder 1";
+
+            e.target.parentNode.insertBefore(folder, createFolderBtn);
+          };
+
+          // render images into div
+          const privateImageRow = document.querySelector(".album .row .images");
+          res.forEach(obj => {
+            const imgHref = `html/label.html?id=${obj.image_id}&src=${obj.image_path}`;
+            const imgPath = obj.image_path;
+            const labelStatus = obj.status ? "labeled" : "unlabeled";
+            const html = `
+                <div class="col-4">
+                    <div class="card shadow-sm">
+                        <a class="image" href=${imgHref}>
+                            <img src=${imgPath} width="100%" height="100%">
+                        </a>
+                        <p>Tag: ${obj.tag}</p>
+                        <p>Status: ${labelStatus}</p>
+                    </div>
+                </div>
+                `;
+            privateImageRow.innerHTML += html;
+          });
+        } else {
+          res.forEach(obj => {
+            const imgHref = `html/label.html?id=${obj.image_id}&src=${obj.image_path}`;
+            const imgPath = obj.image_path;
+            const labelStatus = obj.status ? "labeled" : "unlabeled";
+            const html = `
+                <div class="col-4">
+                    <div class="card shadow-sm">
+                        <a class="image" href=${imgHref}>
+                            <img src=${imgPath} width="100%" height="100%">
+                        </a>
+                        <p>Tag: ${obj.tag}</p>
+                        <p>Status: ${labelStatus}</p>
+                    </div>
+                </div>
+                `;
+            imageRow.innerHTML += html;
+
+            // using css only
+            // const html = `
+            // <div class="picture">
+            //     <p>Tag: ${obj.tag}</p>
+            //     <div class="imageContainer">
+            //         <a class="image" href=${imgHref}>
+            //             <img src=${imgPath} alt="">
+            //         </a>
+            //     </div>
+            //     <p>Download Image: <a target="_blank" href=${imgPath}>Click Here</a></p>
+            // </div>
+            // `;
+            // imagesBox.innerHTML += html;
+          });
+        }
       } else {
         imageRow.innerHTML = "Labeled Image Not Found, try label images now!";
       }
