c84a80d62fc7f64cbcad88e0364cd1159d8d0262
junhong1923 <m81923@gmail.com>
2021-06-21 02:15:23 +0800

    Clean unused middleware in image_route

--- server/routes/image_route.js
+++ server/routes/image_route.js
@@ -1,8 +1,7 @@
 const router = require("express").Router();
-const { upload, uploadS3, authentication } = require("../../util/util");
+const { uploadS3, authentication } = require("../../util/util");
 const { getImages, checkImageMiddleware, saveOriginalImage } = require("../controllers/image_controller");
 
-// const oriUpload = upload.single("OriImage");
 const oriUploadS3 = uploadS3.single("OriImage");
 
 router.route("/images/:type")
